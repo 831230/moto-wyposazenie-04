365a6d2164e4fe5f74531d8350ccc4684143e93e
Tomasz Bielecki <szkoleniegoit@gmail.com>
2023-10-24 13:54:57 +0200

    bugfix change import methodes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React, {lazy} from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 // components imports
-const Home = lazy(() => import("pages/home/Home"));
-const Aboute = lazy(() => import("pages/aboute/Aboute"));
-const Contact = lazy(() => import("pages/contact/Contact"));
-const WebAppTemplate = lazy(() => import("components/webAppTemplate/WebAppTemplate"));
+// const Home = lazy(() => import("pages/home/Home"));
+// const Aboute = lazy(() => import("pages/aboute/Aboute"));
+// const Contact = lazy(() => import("pages/contact/Contact"));
+// const WebAppTemplate = lazy(() => import("components/webAppTemplate/WebAppTemplate"));
 
+import Home from 'pages/home/Home';
+import Aboute from 'pages/aboute/Aboute';
+import Contact from 'pages/contact/Contact';
+import WebAppTemplate from 'components/webAppTemplate/WebAppTemplate';
 
 const App = () => {
-  return ( 
+  return (
     <>
-    <h2>APP</h2>
-    <Routes> 
-      <Route path="/" element={<WebAppTemplate/>}>
-        {/* HOME */}
-        <Route index element={<Home/>}/>
-        <Route path="/o-nas" element={<Aboute/>}/>
-        <Route path="/kontakt" element={<Contact/>}/>
-      </Route>
-    </Routes>
+      <h2>APP</h2>
+      <Routes>
+        <Route path="/" element={<WebAppTemplate />}>
+          {/* HOME */}
+          <Route index element={<Home />} />
+          <Route path="/o-nas" element={<Aboute />} />
+          <Route path="/kontakt" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
-   );
-}
- 
+  );
+};
+
 export default App;
